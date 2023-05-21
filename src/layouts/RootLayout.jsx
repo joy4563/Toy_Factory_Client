482374a4105c0464af64e9468f3fc33a22a7ea50
joy4563 <joymondal4563@gmail.com>
2023-05-21 23:44:13 +0600

    index.js & index.html file modified

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar/Navbar';
 import Footer from '../components/Footer/Footer';
-// import rootBg from '../assets/images/root-bg.jpg';
 import bg from '../assets/images/main-bg.jpg';
 
 const RootLayout = () => {
   return (
     <div
-      className="bg-cover bg-no-repeat bg-top min-h-screen w-full overflow-x-hidden"
-      style={{
-        backgroundImage: `url(${bg})`,
-        backgroundAttachment: 'fixed'
-      }}
+      className="
+        bg-cover 
+        bg-no-repeat 
+        bg-top 
+        min-h-screen 
+        w-full 
+        overflow-x-hidden 
+        bg-fixed
+      "
+      style={{ backgroundImage: `url(${bg})` }}
     >
       <Navbar />
       <Outlet />
